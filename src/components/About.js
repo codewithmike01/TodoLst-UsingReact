8fd6e3e8bd1c51869cca7753d16d8baeb9599a80
ginohmk <michotall95@gmail.com>
2022-02-18 11:22:47 +0100

    test

---
+++ src/components/About.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function About() {
+  return (
+    <div className="about">
+      <h2>About</h2>
+      <p>
+        lorem ipsum cordan mike ipsum Ginoh codana kanu lorem ipsum cordan mike
+        ipsum Ginoh codana kanu lorem ipsum cordan mike ipsum Ginoh codana kanu
+        lorem ipsum cordan mike ipsum Ginoh codana kanu lorem ipsum cordan mike
+        ipsum lorem Ginoh codana kanu lorem ipsum cordan mike ipsum Ginoh codana
+        kanu lorem ipsum cordan mike ipsum Ginoh codana kanu
+      </p>
+    </div>
+  );
+}
